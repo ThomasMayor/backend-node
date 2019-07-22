752824209580c28529892b1bcbd3bdcd41280078
Thomas Mayor <thomas.mayor.ge@gmail.com>
2019-07-22 12:01:44 +0200

    add mongoose & database connection

---
+++ src/database.ts
@@ -0,0 +1,29 @@
+import * as mongoose from 'mongoose';
+import { Mongoose } from 'mongoose';
+
+// remove deprecated warning
+mongoose.set('useFindAndModify', false);
+
+export class Database {
+  private db: Mongoose = mongoose; // mongoose is an instance of class Mongoose
+
+  constructor(
+    private uri: string,
+    private user?: string,
+    private pass?: string,
+  ) { }
+
+  connect() {
+    console.log('Connecting database to ' + this.uri);
+    return this.db
+               .connect(this.uri, { useNewUrlParser: true, user: this.user, pass: this.pass })
+               .then(mongooseConnected => {
+                 console.log('Database connected to ' + this.uri);
+                 return mongooseConnected;
+               });
+  }
+
+  disconnect() {
+    return this.db.disconnect();
+  }
+}
